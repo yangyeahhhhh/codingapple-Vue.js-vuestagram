430218a97b4644bb4615ae11c131a896e0476086
yangyeahhhhh <112038515+yangyeahhhhh@users.noreply.github.com>
2022-11-27 22:06:44 +0900

    2022년 11월 27일

---
+++ vuestagram/src/components/ContainerView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Post :게시물="게시물[i]" v-for="(a, i) in 게시물" :key="i"></Post>
+  </div>
+</template>
+
+<script>
+import Post from "./PostView.vue";
+export default {
+  components: {
+    Post,
+  },
+  props: {
+    게시물: Array,
+  },
+};
+</script>
+
+<style></style>
