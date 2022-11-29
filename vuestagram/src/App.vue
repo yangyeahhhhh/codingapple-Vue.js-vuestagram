4c01295456319820ffb0483b2b9c54a8d1b48cc2
yewon <ipunwon_@naver.com>
2022-11-29 22:05:53 +0900

    2022년 11월 29일

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -11,6 +11,8 @@
 
   <Container :게시물="게시물" />
 
+  <button @click="more">더보기</button>
+
   <div class="footer">
     <ul class="footer-button-plus">
       <input type="file" id="file" class="inputfile" />
@@ -22,17 +24,29 @@
 <script>
 import Container from "./components/ContainerView.vue";
 import postdata from "./assets/postdata.js";
+import axios from 'axios'
 
 export default {
   name: "App",
   data() {
     return {
       게시물: postdata,
+      더보기: 0,
     };
   },
   components: {
     Container,
   },
+  methods: {
+    more(){
+      axios.get(`https://codingapple1.github.io/vue/more${this.더보기}.json`)
+      .then((결과)=>{
+        console.log(결과.data)
+        this.게시물.push(결과.data)
+        this.더보기++;
+      })
+    }
+  }
 };
 </script>
 
