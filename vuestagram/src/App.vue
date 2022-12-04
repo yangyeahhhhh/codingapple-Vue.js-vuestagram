4ffb9e80308673204ddaec74c13d7052617bf440
yangyeahhhhh <112038515+yangyeahhhhh@users.noreply.github.com>
2022-12-04 23:16:31 +0900

    2022년 12월 4일

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -4,21 +4,31 @@
       <li>Cancel</li>
     </ul>
     <ul class="header-button-right">
-      <li>Next</li>
+      <li v-if="step == 1" @click="step++">Next</li>
+      <li v-if="step == 2" @click="publish">발행</li>
     </ul>
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :게시물="게시물" />
+  <Container @write="작성한글 = $event" :게시물="게시물" :step="step" :이미지="이미지"/>
 
   <button @click="more">더보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
+      <input @change="upload" multiple accept="image/*" type="file" id="file" class="inputfile" />
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
+  <!-- <div v-if="step == 0">내용0</div>
+  <div v-if="step == 1">내용1</div>
+  <div v-if="step == 2">내용2</div>
+  <button @click="step = 0">버튼0</button>
+  <button @click="step = 1">버튼1</button>
+  <button @click="step = 2">버튼2</button>
+  <div style="margin-top : 500px;"></div> -->
+
 </template>
 
 <script>
@@ -32,12 +42,29 @@ export default {
     return {
       게시물: postdata,
       더보기: 0,
+      step: 0,
+      이미지: '',
+      작성한글: ''
     };
   },
   components: {
     Container,
   },
   methods: {
+    publish(){
+      var 내게시물 = {
+        name: "Yang yewon",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: this.이미지,
+        likes: 36,
+        date: "May 15",
+        liked: false,
+        content: this.작성한글,
+        filter: "perpetua",
+      };
+      this.게시물.unshift(내게시물);
+      this.step = 0;
+    },
     more(){
       axios.get(`https://codingapple1.github.io/vue/more${this.더보기}.json`)
       .then((결과)=>{
@@ -45,6 +72,14 @@ export default {
         this.게시물.push(결과.data)
         this.더보기++;
       })
+    },
+    upload(e){
+      let 파일 = e.target.files
+      console.log(파일);
+      let url = URL.createObjectURL(파일[0]);
+      console.log(url)
+      this.이미지 = url;
+      this.step++;
     }
   }
 };
